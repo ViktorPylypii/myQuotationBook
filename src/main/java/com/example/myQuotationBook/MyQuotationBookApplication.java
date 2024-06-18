e6547a8a470e539558cf47f603801bf01013c8b6
Viktor <viktorpuluiy@gmail.com>
2024-06-18 14:38:09 +0300

    first commit

---
+++ src/main/java/com/example/myQuotationBook/MyQuotationBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.myQuotationBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQuotationBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyQuotationBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myQuotationBook/MyQuotationBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myQuotationBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyQuotationBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
